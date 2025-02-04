03014ab357bdf4ae681dedf580a64d01a7c9f0c5
Saksorn <saksorn.ruang@gmail.com>
2025-02-04 16:51:22 +0700

    add prod and dotenv

--- gabm_infra/settings/prod.py
+++ gabm_infra/settings/prod.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
