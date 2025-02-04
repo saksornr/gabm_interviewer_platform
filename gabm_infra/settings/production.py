7cc55d216c5a57d1c6a8dc82a44b0653633f3aa2
Saksorn <saksorn.ruang@gmail.com>
2025-02-04 17:07:13 +0700

    add allows host

--- gabm_infra/settings/production.py
+++ gabm_infra/settings/production.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOW_HOST = os.environ.get("ALLOW_HOST", [])
 
 
 # Application definition
