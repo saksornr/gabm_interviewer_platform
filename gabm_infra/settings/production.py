1bc41d8c99b054592283e260a837eca6efcb6777
Saksorn Ruangtanusak <38836072+saksornr@users.noreply.github.com>
2025-02-18 14:25:16 +0700

    Add Databases

--- gabm_infra/settings/production.py
+++ gabm_infra/settings/production.py
@@ -126,11 +126,16 @@ WSGI_APPLICATION = 'gabm_infra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 SITE_ID = 1
